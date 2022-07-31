9fc2e412c53729fc65ffd1832b8e5eacf601ccbe
Haidar Ezio <haidar@haidarezio.me>
2022-07-31 22:41:10 +0100

    added assets for navbar and header

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import tw from "tailwind-styled-components";
-import Hi from "../components/hi";
-import Hello from "../components/hello";
 
 const Container = tw.div`
     flex
@@ -11,15 +9,9 @@ const Container = tw.div`
     justify-center
     flex-col
     w-full
-    bg-green-500
+    text-white
 `;
 
 export default function Home() {
-  return (
-    <Container>
-      <div>Base File to Use Styled Components + Tailwind</div>
-      <Hi />
-      <Hello />
-    </Container>
-  );
+  return <Container>hello</Container>;
 }
